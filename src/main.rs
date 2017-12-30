10a1726caf5817d2435e7a4fe9dcbdda6d90c2f1
Sean <sean.mceligot@gmail.com>
2017-12-29 21:39:55 -0500

    get status

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::process::Command;
 use std::io::Result;
 use std::path::*;
-//use std::io::Error;
 use std::process::Stdio;
 use std::process::Child;
 use std::process::ChildStdout;
 //use std::io::Read;
 //use std::io::BufReader;
 use std::io::{BufRead, BufReader};
+use std::process::ExitStatus;
+
+#[macro_use]
+extern crate serde_json;
+
+//use serde_json;//::{Value,to_string};
 
 fn readout(out:ChildStdout) -> String {
     let mut buf_reader = BufReader::new(out);
@@ -32,28 +37,48 @@ fn diff(_a:&Path, _b:&Path) -> Result<Child>  {
             .stdout(Stdio::piped())
 			.spawn();
 }
-fn read(result:std::io::Result<Child>) -> Option<String> {
-	match result {
-       Ok(v) => { 
-        let maybe_stdout:std::option::Option<std::process::ChildStdout> = v.stdout;
-        match maybe_stdout {
-            std::option::Option::Some(stdout) => std::option::Option::Some(readout(stdout)),
-            std::option::Option::None => None
-        }
-       }
-       Err(_e) => None 
-			//	0 => print!("called `Result::unwrap()` on an `Err` value: {:?}", ""),
-			//	1 => print!("OK")
-			}
-
+fn read(result:std::io::Result<Child>) -> Result<(Result<ExitStatus>, Option<String> )> {
+    match result {
+       std::result::Result::Ok(mut child) => 
+           std::result::Result::Ok(  //return Ok (T,T)
+            ( 
+                child.wait(),
+                match child.stdout {
+                    std::option::Option::Some(stdout) => Some(readout(stdout)),
+                    std::option::Option::None => None
+                }
+           )),
+       std::result::Result::Err(e) => Err(e) 
+    }
+}
+fn result(maybe_stdout:Option<String>) -> serde_json::Value {
+    match maybe_stdout {
+    Some(stdout) => json!({
+	"result": "patch",
+	  "diff": {
+		"args": "diff test.txt /home/sean/test.txt",
+		"returncode": 1,
+		"stderr": "",
+		"stdout": stdout
+	  }}),
+    None => json!({
+	"result": "patch",
+	  "diff": {
+		"args": "diff test.txt /home/sean/test.txt",
+		"returncode": 1,
+		"stderr": "",
+	  }})
+    }
 }
 fn main() {
     let src = Path::new("test.txt");
     let dst = Path::new("/home/sean/test.txt");
     match read(diff(src, dst)) {
-            std::option::Option::Some(stdout) => { println!("{}", stdout)},
-            std::option::Option::None =>  {println!("failed")},
+            Ok(t) => { 
+                let (maybe_status, maybe_stdout) = t;
+                let json = result(maybe_stdout);
+                println!("{}", json.to_string());
+            },
+            Err(e) =>  {println!("error {}", e)}
     }
-
-//			match process.stdout {
 }
