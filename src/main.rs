c8aa2169fd5584c8039a9cf94d9e1eff04471db5
Sean <sean.mceligot@gmail.com>
2017-12-31 13:16:03 -0500

    use map

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::process::Command;
 use std::io::Result;
 use std::path::*;
 use std::process::Stdio;
-use std::process::Child;
+//use std::process::Child;
 use std::process::ChildStdout;
 //use std::io::Read;
 //use std::io::BufReader;
 use std::io::{BufRead, BufReader};
-use std::process::ExitStatus;
+//use std::process::ExitStatus;
+use std::process::Output;
 
 extern crate serde;
 #[macro_use]
@@ -19,7 +20,7 @@ use serde_json::ser::to_string_pretty;
 
 //use serde_json;//::{Value,to_string};
 
-fn readout(out:ChildStdout) -> String {
+fn _childstdout_to_string(out:ChildStdout) -> String {
     let mut buf_reader = BufReader::new(out);
 	//let len = reader.read_line(&mut line)?;
 	let mut line = String::new();
@@ -35,43 +36,28 @@ fn readout(out:ChildStdout) -> String {
     }
     line
 }
-fn diff(_a:&Path, _b:&Path) -> Result<Child>  {
-    Command::new("diff")
+#[derive(Serialize, Deserialize, Debug)]
+struct ExecReturn {
+    stdout: String,
+    stderr: String,
+    exit_code: Option<i32>
+}
+fn output_to_execresult(output:Output) -> ExecReturn {
+    let o:Vec<u8> = output.stdout;
+    let e:Vec<u8> = output.stderr;
+    ExecReturn { 
+        stdout: String::from_utf8(o).unwrap(),
+        stderr: String::from_utf8(e).unwrap(),
+        exit_code: output.status.code() 
+   } 
+}
+fn diff(_a:&Path, _b:&Path) -> Result<ExecReturn>  {
+    let result_output= Command::new("diff")
             .arg("test.txt")
             .arg("/home/sean/test.txt")
             .stdout(Stdio::piped())
-			.spawn()
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct ExecResult {
-    stdout: Option<String>,
-    stderr: Option<String>,
-    exit_code: Option<i32>
-}
-
-fn read_command_child(result_child:std::io::Result<Child>) -> Result<(Result<ExitStatus>, Option<String> )> {
-    match result_child {
-       std::result::Result::Ok(mut child) => 
-           std::result::Result::Ok(  //return Ok (T,T)
-            ( 
-                child.wait(),
-                //match child.stdout { std::option::Option::Some(stdout) => Some(readout(stdout)), std::option::Option::None => None }
-                child.stdout.map(readout)
-           )),
-       std::result::Result::Err(e) => Err(e) 
-    }
-}
-fn diff_result_json(optional_stdout:Option<String>) -> serde_json::Value {
-    let mut result = json!({ });
-
-    match optional_stdout {
-        Some(stdout) => {
-            result["stdout"] = json!(stdout);
-            result
-        },
-        None => result
-    }
+			.output();
+    result_output.map( output_to_execresult )
 }
 
 //fn json_or_err<'a>(tag:&'a str, option_val:Option<&'a str>)  -> (&'a str,Option<&'a str>) {
@@ -95,14 +81,17 @@ fn input_json(mut parent:serde_json::Value, src:&Path, dst:&Path) -> serde_json:
     }
     parent
 }
+fn difftask(src:String,dst:String) {
+}
+
 fn main() {
     let src = Path::new("test.txt");
     let dst = Path::new("/home/sean/test.txt");
     let r1 = json!({});
     let mut r2 = input_json(r1, src, dst);
     println!("input {}", r2.to_string());
-    r2["result"] = match read_command_child(diff(src, dst)) {
-            Ok( t ) => diff_result_json(t.1) ,
+    r2["result"] = match diff(src, dst) {
+            Ok( t ) => json!(t) ,
             Err(e) =>  json!({"error!": format!("{}", e)})
     };
     println!("result {}", to_string_pretty(&r2).unwrap());
